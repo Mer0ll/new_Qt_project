e4ff321e012e4e211950dc222de32265d49ca385
Mer0ll <pony1890@gmail.com>
2022-12-08 20:28:11 +0300

    08.12.22__20:28

--- Практика 3/a_Повторение/a_qtimer_repeat.py
+++ Практика 3/a_Повторение/a_qtimer_repeat.py
@@ -14,13 +14,77 @@
 Опционально поработать с валидацией url
 """
 
-from PySide6 import QtWidgets
+import time
+
+import requests
+from PySide6 import QtWidgets, QtCore
+
+
+class Worker(QtCore.QThread):
+    progress = QtCore.Signal(int)
+    url = None
+
+    def start(self, *args, url=None, **kwargs):
+        self.url = url
+        return super().start(*args, **kwargs)
+
+    def run(self) -> None:
+        """
+        Метод имитирующий долгую задачу
+
+        :return: None
+        """
+
+        data = requests.get(self.url)
+        self.progress.emit(data.status_code)
+        self.finished.emit()
 
 
 class Window(QtWidgets.QWidget):
+    class Window(QtWidgets.QWidget):
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
+        def __init__(self, parent=None):
+            super().__init__(parent)
+            self.initUi()
+            self.initSignals()
+            self.initTread()
+
+
+        def initUi(self):
+            self.line_1 = QtWidgets.QLineEdit()
+            self.line_1.setPlaceholderText('Адрес')
+            self.button1 = QtWidgets.QPushButton('Проверить!')
+            self.button2 = QtWidgets.QPushButton('Очистить!')
+            self.layout1 = QtWidgets.QHBoxLayout()
+            self.layout1.addWidget(self.line_1)
+            self.layout1.addWidget(self.button1)
+            self.textP = QtWidgets.QPlainTextEdit()
+            self.mainlayout = QtWidgets.QVBoxLayout()
+            self.mainlayout.addLayout(self.layout1)
+            self.mainlayout.addWidget(self.textP)
+            self.mainlayout.addWidget(self.button2)
+
+            self.setLayout(self.mainlayout)
+
+        def initSignals(self):
+            self.button1.clicked.connect(self.startProcess)
+            self.button2.clicked.connect(self.textP.clear)
+
+            self.thread.progress.connect(self.reportProgress)
+            self.thread.finished.connect(lambda: self.button1.setEnabled(True))
+            self.thread.finished.connect(self.thread.quit)
+
+        def initTread(self):
+            self.thread = Worker()
+
+        @QtCore.Slot()
+        def startProcess(self):
+            self.button1.setEnabled(False)
+            self.thread.start(url=self.line_1.text())
+
+        @QtCore.Slot()
+        def reportProgress(self, progres):
+            self.textP.appendPlainText(str(progres))
 
 
 if __name__ == "__main__":
