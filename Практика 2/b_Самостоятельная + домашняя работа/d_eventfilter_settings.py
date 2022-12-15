ceb2f593f896a2d2c5cc8755a71a01c3911d67f0
Mer0ll <pony1890@gmail.com>
2022-12-15 20:56:20 +0300

    15.12.2022______20:56

--- Практика 2/b_Самостоятельная + домашняя работа/d_eventfilter_settings.py
+++ Практика 2/b_Самостоятельная + домашняя работа/d_eventfilter_settings.py
@@ -30,10 +30,14 @@ class Window(QtWidgets.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.initSignals()
+        self.ui.comboBox.addItems(['oct', 'hex', 'bin', 'dec'])
 
     def initSignals(self):
         self.ui.dial.valueChanged.connect(self.setDataLSD1)
         self.ui.horizontalSlider.valueChanged.connect(self.setDataLSD2)
+
+        # self.ui.comboBox.valueChanged.connect(self.setLCDmod)
+        self.ui.comboBox.currentTextChanged.connect(self.setLCDmod)
 
     def setDataLSD1(self):
         self.ui.lcdNumber.display(self.ui.dial.value())
@@ -42,6 +46,16 @@ class Window(QtWidgets.QWidget):
     def setDataLSD2(self):
         self.ui.lcdNumber.display(self.ui.horizontalSlider.value())
         self.ui.dial.setValue(self.ui.horizontalSlider.value())
+
+    def setLCDmod(self):
+        if self.ui.comboBox.currentText() == 'hex':
+            self.ui.lcdNumber.setHexMode()
+        if self.ui.comboBox.currentText() == 'dec':
+            self.ui.lcdNumber.setDecMode()
+        if self.ui.comboBox.currentText() == 'oct':
+            self.ui.lcdNumber.setOctMode()
+        if self.ui.comboBox.currentText() == 'bin':
+            self.ui.lcdNumber.setBinMode()
 
 
 if __name__ == "__main__":
